cbba98b2af63859bb5f4def91bc288c4f7385c18
SabaMAkki <s.sabamakki@gmail.com>
2022-07-16 14:43:30 +0430

    add Welcome component

---
+++ components/WelcomeC.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-row justify="center" align="center">
+        <v-img
+          src="https://bad.src/not/valid"
+          lazy-src="https://picsum.photos/id/11/100/60"
+          max-width="500"
+          max-height="500"
+        >
+        </v-img>
+      </v-row>
+    </v-row>
+    <v-row justify="center" align="center">
+      <p>welcome</p>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-btn depressed color="primary"> Login </v-btn>
+      <v-btn depressed color="primary"> Signin </v-btn>
+    </v-row>
+  </v-container>
+</template>
