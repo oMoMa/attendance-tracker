a0bb4d00d624f0a9aecfc16e1054dab573494b3c
SabaMAkki <s.sabamakki@gmail.com>
2022-07-29 14:05:18 +0430

    make card's style

--- components/ReportCo.vue
+++ components/ReportCo.vue
@@ -1,60 +1,80 @@
 <template>
   <div>
-    <v-card class="ma-2">
+    <v-card class="ma-3">
       <v-container fluid grid-list-lg>
         <v-layout row wrap>
           <v-flex xs12>
-            <v-card color="blue-grey darken-2" class="white--text">
-              <v-card-title primary-title>
-                <div>
-                  <div class="headline text-right">حضور</div>
-                </div>
-              </v-card-title>
+            <v-card color="green darken-3 darken-2" class="white--text">
+              <div class="pt-5 pb-5">
+                <v-row class="mb-6" no-gutters>
+                  <v-col sm="5" md="6" class="pl-8"
+                    ><big> ساعت 00 و 00 دقیقه </big></v-col
+                  >
+                  <v-col sm="5" offset-sm="2" md="6" offset-md="0">
+                    <div class="headline pl-16">حضور</div>
+                  </v-col>
+                </v-row>
+              </div>
             </v-card>
           </v-flex>
         </v-layout>
       </v-container>
     </v-card>
-    <v-card class="ma-2">
+    <v-card class="ma-3">
       <v-container fluid grid-list-lg>
         <v-layout row wrap>
           <v-flex xs12>
-            <v-card color="blue-grey darken-2" class="white--text">
-              <v-card-title primary-title>
-                <div>
-                  <div class="headline float-right">تاخیر</div>
-                </div>
-              </v-card-title>
+            <v-card color="deep-orange darken-4 darken-2" class="white--text">
+              <div class="pt-5 pb-5">
+                <v-row class="mb-6" no-gutters>
+                  <v-col sm="5" md="6" class="pl-8"
+                    ><big> ساعت 00 و 00 دقیقه </big></v-col
+                  >
+                  <v-col sm="5" offset-sm="2" md="6" offset-md="0">
+                    <div class="headline pl-16">غیبت</div>
+                  </v-col>
+                </v-row>
+              </div>
             </v-card>
           </v-flex>
         </v-layout>
       </v-container>
     </v-card>
-    <v-card class="ma-2">
+    <v-card class="ma-3">
       <v-container fluid grid-list-lg>
         <v-layout row wrap>
           <v-flex xs12>
-            <v-card color="blue-grey darken-2" class="white--text">
-              <v-card-title primary-title>
-                <div>
-                  <div class="headline">غیبت</div>
-                </div>
-              </v-card-title>
+            <v-card color="orange darken-4 darken-2" class="white--text">
+              <div class="pt-5 pb-5">
+                <v-row class="mb-6" no-gutters>
+                  <v-col sm="5" md="6" class="pl-8"
+                    ><big> ساعت <span>00</span> و 00 دقیقه </big></v-col
+                  >
+                  <v-col sm="5" offset-sm="2" md="6" offset-md="0">
+                    <div class="headline pl-16">تاخیر</div>
+                  </v-col>
+                </v-row>
+              </div>
             </v-card>
           </v-flex>
         </v-layout>
       </v-container>
     </v-card>
-    <v-card class="ma-2">
+    <v-card class="ma-3">
       <v-container fluid grid-list-lg>
         <v-layout row wrap>
           <v-flex xs12>
-            <v-card color="blue-grey darken-2" class="white--text">
-              <v-card-title primary-title>
-                <div>
-                  <div class="headline">اضافه کار</div>
-                </div>
-              </v-card-title>
+            <v-card color="blue darken-3" class="white--text">
+              <div class="pt-5 pb-5">
+                <v-row class="mb-6" no-gutters>
+                  <v-col sm="5" md="6" class="pl-8"
+                    ><big> ساعت 00 و 00 دقیقه </big></v-col
+                  >
+                  <v-col sm="5" offset-sm="2" md="6" offset-md="0">
+                    <div class="headline pl-13">اضافه کار</div>
+                  </v-col>
+                </v-row>
+              </div>
             </v-card>
           </v-flex>
         </v-layout>
@@ -62,3 +82,9 @@
     </v-card>
   </div>
 </template>
+
+<style scoped>
+.v-application .mb-6 {
+  margin-bottom: 0px;
+}
+</style>
