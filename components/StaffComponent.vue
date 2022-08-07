c0c302961fdb7694a47ddc70dfe07e3b8dde4f27
SabaMAkki <s.sabamakki@gmail.com>
2022-08-07 17:38:20 +0430

    table

---
+++ components/StaffComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="users"
+    hide-default-header
+    hide-default-footer
+    class="elevation-1"
+  ></v-data-table>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: 'users',
+          align: 'start',
+          value: 'name',
+        },
+      ],
+      users: [
+        {
+          name: 'Evan You',
+        },
+        {
+          name: 'Jeff Bezos',
+        },
+        {
+          name: 'StackOverflow',
+        },
+        {
+          name: 'Steve Jobs',
+        },
+        {
+          name: 'Youtube',
+        },
+      ],
+    }
+  },
+}
+</script>
