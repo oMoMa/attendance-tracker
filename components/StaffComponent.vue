6b800e1e849c6522029e6c8470bdbe908a617684
SabaMAkki <s.sabamakki@gmail.com>
2022-08-07 19:02:33 +0430

    show list of staff

--- components/StaffComponent.vue
+++ components/StaffComponent.vue
@@ -1,16 +1,60 @@
+
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="users"
-    hide-default-header
-    hide-default-footer
-    class="elevation-1"
-  ></v-data-table>
+  <div>
+    <v-row class="mt-5">
+      <v-col sm="12">
+        <!-- <v-row no-gutters>
+          <v-col justify="center" align="center" cols="4" sm="6">
+            <v-btn></v-btn>
+          </v-col>
+          <v-col justify="center" align="center" cols="8" sm="6">
+            <v-btn></v-btn>
+          </v-col>
+        </v-row> -->
+
+        <v-select
+          justify="center"
+          align="center"
+          class="mt-5"
+          :items="items"
+          label="کارکنان حاضر"
+          solo
+        ></v-select>
+      </v-col>
+    </v-row>
+
+    <v-card>
+      <v-simple-table class="mt-3">
+        <template #default>
+          <!-- <thead>
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-left">Calories</th>
+        </tr>
+      </thead> -->
+          <tbody>
+            <tr v-for="user in users" :key="user.id">
+              <td justify="center" align="center" class="pa-2">
+                <v-avatar class="mr-8">
+                  <img alt="Avatar" :src="require('assets/Images/t.jpg')" />
+                </v-avatar>
+              </td>
+              <td justify="center" align="center" class="pa-2">
+                {{ user.name }}
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-card>
+  </div>
 </template>
+
 <script>
 export default {
   data() {
     return {
+      items: ['همه کارکنان', 'کارکنان حاضر'],
       headers: [
         {
           text: 'users',
@@ -20,18 +64,63 @@ export default {
       ],
       users: [
         {
+          id: 1,
           name: 'Evan You',
         },
         {
+          id: 2,
           name: 'Jeff Bezos',
         },
         {
+          id: 3,
           name: 'StackOverflow',
         },
         {
+          id: 4,
           name: 'Steve Jobs',
         },
         {
+          id: 5,
+          name: 'Youtube',
+        },
+        {
+          id: 1,
+          name: 'Evan You',
+        },
+        {
+          id: 2,
+          name: 'Jeff Bezos',
+        },
+        {
+          id: 3,
+          name: 'StackOverflow',
+        },
+        {
+          id: 4,
+          name: 'Steve Jobs',
+        },
+        {
+          id: 5,
+          name: 'Youtube',
+        },
+        {
+          id: 1,
+          name: 'Evan You',
+        },
+        {
+          id: 2,
+          name: 'Jeff Bezos',
+        },
+        {
+          id: 3,
+          name: 'StackOverflow',
+        },
+        {
+          id: 4,
+          name: 'Steve Jobs',
+        },
+        {
+          id: 5,
           name: 'Youtube',
         },
       ],
